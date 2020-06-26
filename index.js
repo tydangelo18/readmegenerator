23a8f71dc868c602092a2404ec635cfb9308d25b
Tee Coder <tydangelo18@gmail.com>
2020-06-26 17:40:47 -0500

    Added appendFile function.

--- index.js
+++ index.js
@@ -42,3 +42,100 @@ const questions = [
       "How would a user report an issue or make contributions to this project?",
   },
 ];
+
+async function init() {
+ await inquirer.prompt(questions).then((response) => {
+    fs.appendFileAsync(
+      "README.md",
+      "# " + response.projectName + "/n",
+      function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Created!");
+        }
+      }
+    );
+
+    fs.appendFileAsync(
+      "README.md",
+      "# " + response.projectDesc + "/n",
+      function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Created!");
+        }
+      }
+    );
+
+    fs.appendFileAsync(
+      "README.md",
+      "# " + response.projectInstall + "/n",
+      function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Created!");
+        }
+      }
+    );
+
+    fs.appendFilAsync("README.md", "# " + response.projectUse + "/n", function (
+      err
+    ) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Created!");
+      }
+    });
+
+    fs.appendFileAsync(
+      "README.md",
+      "# " + response.projectLicense + "/n",
+      function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Created!");
+        }
+      }
+    );
+
+    fs.appendFileAsync(
+      "README.md",
+      "# " + response.githubName + "/n",
+      function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Created!");
+        }
+      }
+    );
+
+    fs.appendFileAsync(
+      "README.md",
+      "# " + response.projectTest + "/n",
+      function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Created!");
+        }
+      }
+    );
+  });
+}
+
+init();
+
+console.log(repsonse);
+console.log(repsonse.projectName);
+console.log(repsonse.projectDesc);
+console.log(repsonse.projectInstall);
+console.log(repsonse.projectUse);
+console.log(repsonse.projectLicense);
+console.log(repsonse.githubName);
+console.log(repsonse.projectTest);
